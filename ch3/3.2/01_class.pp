4ad37aeec2b8fec0babfb799dcc42667c4976f3c
yinm <yinm.cminor9@gmail.com>
2017-10-08 12:02:05 +0900

    Check singleton class

---
+++ ch3/3.2/01_class.pp
@@ -0,0 +1,21 @@
+class system_user (String $username) {
+  user {$username:
+    ensure => present,
+    shell => "/bin/bash",
+    managehome => true,
+  }
+  file {'create_profile':
+    ensure => present,
+    path => "/home/${username}/.bash_profile",
+    content => 'export PATH=/bin:/usr/bin',
+    require => User[$username],
+  }
+}
+
+class {'system_user':
+  username => 'gihyo',
+}
+
+class {'system_user':
+  username => 'webdb',
+}
