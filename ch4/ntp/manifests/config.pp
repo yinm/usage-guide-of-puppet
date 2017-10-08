7c6065286f502d523abfc083f67baad572f8fc13
yinm <yinm.cminor9@gmail.com>
2017-10-08 13:25:52 +0900

    Add files

--- ch4/ntp/manifests/config.pp
+++ ch4/ntp/manifests/config.pp
@@ -1,11 +1,7 @@
 class ntp::config {
   file {'/etc/ntp.conf':
     ensure => file,
-    content => @("NTPCONF"/L)
-    driftfile /var/lib/ntp/ntp.drift
-    server 192.168.1.1
-    |NTPCONF
-    ,
+    source => 'puppet:///module/ntp/ntp.conf',
     require => Package['ntp'],
   }
 }
