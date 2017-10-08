aa8250170770ed9ba21c1ac3a049969d43994d3a
yinm <yinm.cminor9@gmail.com>
2017-10-08 11:08:17 +0900

    Add class

---
+++ ch3/3.1/01/ntp_class.pp
@@ -0,0 +1,20 @@
+class ntp {
+  package {'ntp':
+    ensure => installed,
+  }
+  file {'/etc/ntp.conf':
+    ensure => file,
+    content => @("NTPCONF"/L)
+    driftfile /var/lib/ntp.drift
+    server 192.168.1.1
+    |NTPCONF
+    ,
+    require => Package['ntp'],
+  }
+  service {'ntpd':
+    ensure => running,
+    enable => true,
+    hasrestart => true,
+    subscribe => File['/etc/ntp.conf'],
+  }
+}
