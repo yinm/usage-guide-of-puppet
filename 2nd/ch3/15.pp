a714e9e9488b01395672b7d44e49134e3cb4332a
yinm <yinm.cminor9@gmail.com>
2017-10-09 07:52:39 +0900

    Add 3.2

---
+++ 2nd/ch3/15.pp
@@ -0,0 +1,22 @@
+define system_user (String $username) {
+  user {$username:
+    ensure => present,
+    shell => '/bin/bash',
+    managehome => true,
+  }
+
+  file {'create_profile':
+    ensure => present,
+    path => "/home/${username}/.bash_profile",
+    content => 'export PATH=/bin:/usr/bin',
+    require => User[$username],
+  }
+}
+
+system_user {'gihyo':
+  username => 'gihyo',
+}
+
+system_user {'webdb':
+  username => 'webdb',
+}
