fe2c060a931ab5f16a1c674d4ab9f745d1aa5466
yinm <yinm.cminor9@gmail.com>
2017-10-09 08:49:33 +0900

    Use template

--- 2nd/ch4/ntp/manifests/config.pp
+++ 2nd/ch4/ntp/manifests/config.pp
@@ -1,7 +1,7 @@
 class ntp::config {
   file {'/etc/ntp.conf':
     ensure => file,
-    source => puppet:///module/ntp/ntp.conf,
+    content => epp('ntp/ntp.conf.epp', {'server' => '192.168.1.1'}),
     require => Package['ntp'],
   }
 }
